e6ece4a4abe570d602062c7e18b3f2cc57b24929
Bernardo Baquero Stand <baquerober@gmail.com>
2013-01-26 02:35:45 -0430

    Major changes / refactoring
    Too sleepy right now to get into details...

--- js/poppp.js
+++ js/poppp.js
@@ -1,89 +1,56 @@
-$(document).ready(function() {
+(function(win) {
 
-    var shotTemplate = "{{#shots}}<article class='shotWrap'><div class='shot' style='background-image: url({{image_teaser_url}})'></div></article>{{/shots}}";
-
-    var sliderTemplate = "<div id='slider'><div>{{#shots}}<div><div><img class='shotImage' data-img='{{image_url}}' src='{{image_teaser_url}}' /><p data-url='{{short_url}}'>{{title}}</p></div></div>{{/shots}}</div></div>";
-
-    var page = 1,
+    var doc = win.document,
+        body = doc.body,
+        shotTemplate = "{{#shots}}<article class='shotWrap'><div class='shot' style='background-image: url({{image_teaser_url}})'></div></article>{{/shots}}",
+        page = 1,
         perPage = 30,
-        ancho = 320,
+        ancho = doc.body.offsetWidth,
         activeView = 1,
         slider;
 
     var loadShots = function() {
-            var main = $("#wrapper");
-            main.append("<p class='loading'>Cargando shots...</p>");
+            var main = $("#mainWrap");
+            main.append("<p class='loading'>Loading shots...</p>");
             $.getJSON("//api.dribbble.com/shots/popular?page=" + page + "&per_page=" + perPage + "&callback=?", function(data) {
                 $(".loading").remove();
                 var html = Mustache.to_html(shotTemplate, data);
                 main.append(html);
-                var sliderHtml = Mustache.to_html(sliderTemplate, data);
-                $("#detailView").append(sliderHtml);
-                slider = new Swipe(document.getElementById('slider'), {
-                    callback: function(e, pos) {
-                        var s = $('.shotImage[data-id="' + pos + '"]');
-                        var url = s.attr("data-img");
-                        s.attr("src", url);
-                    }
-                });
+
                 var lista = $(".shotWrap");
                 $.each(lista, function(i, s) {
                     $(s).attr("data-shot", i);
-                });
-                var shots = $(".shotImage");
-                $.each(shots, function(i, z) {
-                    $(z).attr("data-id", i);
                 });
                 page++;
             });
         };
 
-    window.applicationCache.addEventListener("updateready", function(e) {
-        var update = window.confirm("Update descargado. Recargar para actualizar?");
-        if(update) {
-            window.location.reload();
-        }
-    });
-
-    $("header").on('touchmove', function(e) {
-        e.preventDefault();
-    }, false);
-
+    // Taps
     tappable(".shotWrap", {
         onTap: function(e, target) {
             var id = $(target).attr("data-shot");
-            slider.slide(parseInt(id), 100);
             slideFromRight();
-            ensenar("#navBack");
+            var button = $("#navBack");
+            button.removeClass('hide');
+            setTimeout(function() {
+                button.removeClass('invisible');
+            }, 10);
         }
     });
 
     tappable("#navBack", {
         onTap: function(e, target) {
-            $(target).addClass("invisible");
+            var button = $(target);
+            button.addClass("invisible");
+            setTimeout(function() {
+                button.addClass("hide");
+            }, 351);
             slideFromLeft();
         }
     });
 
-    tappable("#slider div div div p", {
-        onTap: function(e, target) {
-            var open = window.confirm("Abrir este Shot de Dribble en Safari?");
-            if(open) {
-                url = $(target).attr("data-url");
-                var a = document.createElement('a');
-                a.setAttribute("href", url);
-                a.setAttribute("target", "_blank");
-
-                var dispatch = document.createEvent("HTMLEvents");
-                dispatch.initEvent("click", true, true);
-                a.dispatchEvent(dispatch);
-            }
-        },
-        activeClass: "title-active"
-    });
-
     // Animaciones
-    var slideFromLeft = function() {
+    var slideFromLeft = function() { // >>>
             var main = $("#mainView");
             var det = $("#detailView");
             main.css("left", -ancho);
@@ -100,12 +67,12 @@ $(document).ready(function() {
                         "left": ""
                     }).removeClass("slideTransition");
                     sacar("#detailView");
-                }, 350);
+                }, 351);
             }, 50);
             activeView = 1;
         };
 
-    var slideFromRight = function() {
+    var slideFromRight = function() { // <<<
             var main = $("#mainView");
             var det = $("#detailView");
             det.css("left", ancho);
@@ -129,20 +96,7 @@ $(document).ready(function() {
             }, 100);
         };
 
-    var reveal = function(element) {
-            var el = $(element);
-            el.removeClass("fuera").addClass("invisible");
-            setTimeout(function() {
-                el.removeClass("invisible");
-            });
-        };
-
     // Metodos de vistas
-    var mostrar = function(element) {
-            var el = $(element);
-            el.removeClass("oculto");
-        };
-
     var sacar = function(element) {
             var el = $(element);
             el.addClass("fuera");
@@ -154,24 +108,14 @@ $(document).ready(function() {
             return el;
         };
 
-    var ocultar = function(element) {
-            var el = $(element);
-            el.addClass("oculto");
-        };
-
     var ensenar = function(element) {
             var el = $(element);
             el.removeClass("invisible");
         };
 
-    loadShots();
-
-    var d = document,
-        body = d.body;
-
     var supportOrientation = typeof window.orientation !== 'undefined',
         getScrollTop = function() {
-            return window.pageYOffset || d.compatMode === 'CSS1Compat' && d.documentElement.scrollTop || body.scrollTop || 0;
+            return window.pageYOffset || doc.compatMode === 'CSS1Compat' && doc.documentElement.scrollTop || body.scrollTop || 0;
         },
         scrollTop = function() {
             if(!supportOrientation) return;
@@ -184,6 +128,19 @@ $(document).ready(function() {
             }, 1);
         };
 
+    // Launch
+    loadShots();
     scrollTop();
 
-});
+    $("header").on('touchmove', function(e) {
+        e.preventDefault();
+    }, false);
+
+    window.applicationCache.addEventListener("updateready", function(e) {
+        var update = window.confirm("Update descargado. Recargar para actualizar?");
+        if(update) {
+            window.location.reload();
+        }
+    });
+
+})(window);
