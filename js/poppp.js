283120d006c86d60fddb7a7182a69c9fc6196b15
Bernardo Baquero Stand <baquerober@gmail.com>
2013-02-22 00:13:32 -0430

    Changed View global var name...

--- js/poppp.js
+++ js/poppp.js
@@ -11,31 +11,31 @@
         imgWidth, imgHeigth, lastData, currentColumn = 'two',
         currentShotURL;
 
-    var View = {
+    var V = { // View
         Action: {
             gotToMainSection: function() {
-                View.Section.headerTitle.text('Poppp').removeClass('title-shot').addClass('title-main');
-                View.Button.back.addClass("invisible"), View.Button.open.addClass("invisible");
-                View.Button.menu.removeClass('hide'), View.Button.refresh.removeClass('hide');
+                V.Section.headerTitle.text('Poppp').removeClass('title-shot').addClass('title-main');
+                V.Button.back.addClass("invisible"), V.Button.open.addClass("invisible");
+                V.Button.menu.removeClass('hide'), V.Button.refresh.removeClass('hide');
                 setTimeout(function() {
-                    View.Button.back.addClass("hide"), View.Button.open.addClass('hide');
-                    View.Button.menu.removeClass('invisible'), View.Button.refresh.removeClass('invisible');
+                    V.Button.back.addClass("hide"), V.Button.open.addClass('hide');
+                    V.Button.menu.removeClass('invisible'), V.Button.refresh.removeClass('invisible');
                 }, 351);
-                View.Animation.slideFromLeft();
+                V.Anims.slideFromLeft();
             },
             goToShotDetailSection: function() {
-                View.Animation.slideFromRight();
-                View.Button.back.removeClass('hide'), View.Button.open.removeClass('hide');
-                View.Button.menu.addClass('invisible'), View.Button.refresh.addClass('invisible');
-                View.Misc.setMinImgSize();
+                V.Anims.slideFromRight();
+                V.Button.back.removeClass('hide'), V.Button.open.removeClass('hide');
+                V.Button.menu.addClass('invisible'), V.Button.refresh.addClass('invisible');
+                V.Misc.setMinImgSize();
                 setTimeout(function() {
-                    View.Button.back.removeClass('invisible'), View.Button.open.removeClass('invisible');
-                    View.Button.menu.addClass('hide'), View.Button.refresh.addClass('hide');
-                    if(!isDesktop) View.Misc.scrollFixDetail();
+                    V.Button.back.removeClass('invisible'), V.Button.open.removeClass('invisible');
+                    V.Button.menu.addClass('hide'), V.Button.refresh.addClass('hide');
+                    if(!isDesktop) V.Misc.scrollFixDetail();
                 }, 200);
             },
             setHeaderShotTitle: function(shotID) {
-                View.Section.headerTitle.text(shots[shotID].title).addClass('title-shot').removeClass('title-main');
+                V.Section.headerTitle.text(shots[shotID].title).addClass('title-shot').removeClass('title-main');
             },
             toggleMenu: function(show) {
                 showingMenu = show ? 0 : 1;
@@ -43,8 +43,10 @@
             }
         },
         Section: {
-            main: $("#mainView"),
-            detail: $("#detailView"),
+            mainView: $("#mainView"),
+            detailView: $("#detailView"),
+            mainWrap: $("#mainWrap"),
+            detailWrap: $("#detailWrap"),
             headerTitle: $(".title"),
             header: $("header")
         },
@@ -54,45 +56,49 @@
             menu: $('#show-menu'),
             refresh: $('#refresh')
         },
-        Animation: {
+        Anims: {
             slideFromRight: function() { // <<<
-                View.Misc.getWidth();
-                View.Section.detail.css("left", ancho);
+                V.Misc.getWidth();
+                var main = V.Section.mainView,
+                    detail = V.Section.detailView;
+                detail.css("left", ancho);
                 setTimeout(function() {
                     var translate = 'translate3d(-' + ancho + 'px, 0px, 0px)';
                     var cssTransform = {
                         '-webkit-transform': translate,
                         'transform': translate
                     };
-                    View.Section.main.addClass("slideTransition").css(cssTransform);
-                    View.Section.detail.addClass("slideTransition").css(cssTransform);
+                    main.addClass("slideTransition").css(cssTransform);
+                    detail.addClass("slideTransition").css(cssTransform);
                     setTimeout(function() { // Quita las propiedades de transition
                         var cssTransformBack = {
                             '-webkit-transform': '',
                             'transform': ''
                         };
-                        View.Section.detail.css("left", 0).removeClass("slideTransition").removeClass("fuera").css(cssTransformBack);
-                        View.Section.main.removeClass("slideTransition").addClass("fuera").css(cssTransformBack);
+                        detail.css("left", 0).removeClass("slideTransition").removeClass("fuera").css(cssTransformBack);
+                        main.removeClass("slideTransition").addClass("fuera").css(cssTransformBack);
                         activeView = 2;
                     }, 351);
                 }, 100);
             },
             slideFromLeft: function() { // >>>
-                View.Misc.getWidth();
-                View.Section.main.css("left", -ancho);
+                V.Misc.getWidth();
+                var main = V.Section.mainView,
+                    detail = V.Section.detailView;
+                main.css("left", -ancho);
                 setTimeout(function() {
-                    View.Section.main.addClass("slideTransition").css('-webkit-transform', 'translate3d(' + ancho + 'px, 0px, 0px)');
-                    View.Section.detail.addClass("slideTransition").css('-webkit-transform', 'translate3d(' + ancho + 'px, 0px, 0px)');
+                    main.addClass("slideTransition").css('-webkit-transform', 'translate3d(' + ancho + 'px, 0px, 0px)');
+                    detail.addClass("slideTransition").css('-webkit-transform', 'translate3d(' + ancho + 'px, 0px, 0px)');
                     setTimeout(function() {
-                        View.Section.main.removeClass("slideTransition").css({
+                        main.removeClass("slideTransition").css({
                             "-webkit-transform": "",
                             "left": ""
                         }).removeClass("fuera");
-                        View.Section.detail.css({
+                        detail.css({
                             "-webkit-transform": "",
                             "left": ""
                         }).removeClass("slideTransition");
-                        View.Section.detail.addClass("fuera");
+                        detail.addClass("fuera");
                     }, 351);
                 }, 50);
                 activeView = 1;
@@ -108,7 +114,7 @@
         },
         Misc: {
             setMinImgSize: function() {
-                var totalWidth = View.Misc.getWidth();
+                var totalWidth = V.Misc.getWidth();
                 imgWidth = totalWidth - 10; // 10 pixels of side padding - this should be dinamically obtained (todo).
                 imgHeigth = imgWidth * 0.75;
                 $('#detail-image > img').css({
@@ -156,7 +162,7 @@
         showShots: function(data) {
             if(!data) data = lastData;
             $(".main-message").remove();
-            var html = Mustache.to_html(View.Template.getMainTemplate(currentColumn), data);
+            var html = Mustache.to_html(V.Template.getMainTemplate(currentColumn), data);
             $("#mainWrap").append(html);
 
             var loadedShots = data.shots;
@@ -175,23 +181,23 @@
     // Taps
     tappable(".shot", {
         onTap: function(e, target) {
-            if(showingMenu) View.Action.toggleMenu(showingMenu);
+            if(showingMenu) V.Action.toggleMenu(showingMenu);
             var id = $(target).data("shot-id"),
-                det = $('#detailWrap');
+                det = V.Section.detailWrap;
 
-            var html = Mustache.to_html(View.Template.detail, shots[id]);
+            var html = Mustache.to_html(V.Template.detail, shots[id]);
             det.html(html);
 
             currentShotURL = shots[id].url;
 
-            View.Action.setHeaderShotTitle(id);
-            View.Action.goToShotDetailSection();
+            V.Action.setHeaderShotTitle(id);
+            V.Action.goToShotDetailSection();
         }
     });
 
     tappable(".load-more", {
         onTap: function(e, target) {
-            if(showingMenu) View.Action.toggleMenu(showingMenu);
+            if(showingMenu) V.Action.toggleMenu(showingMenu);
             $(target).remove();
             Poppp.loadShots(true); // loadingMore = true
         },
@@ -201,7 +207,7 @@
     tappable("#nav-back", {
         onTap: function(e, target) {
             currentShotURL = '';
-            View.Action.gotToMainSection();
+            V.Action.gotToMainSection();
         },
         activeClass: 'btn-active'
     });
@@ -210,7 +216,7 @@
         onTap: function() {
             currentPage = 1;
             $('#mainWrap').empty();
-            if(showingMenu) View.Action.toggleMenu(showingMenu);
+            if(showingMenu) V.Action.toggleMenu(showingMenu);
             Poppp.loadShots();
         },
         activeClass: 'btn-active'
@@ -218,7 +224,7 @@
 
     tappable("#show-menu", {
         onTap: function() {
-            if(activeView === 1) View.Action.toggleMenu(showingMenu);
+            if(activeView === 1) V.Action.toggleMenu(showingMenu);
         },
         activeClass: 'btn-active'
     });
@@ -227,7 +233,7 @@
         onTap: function(e, target) {
             var choice = $(target),
                 choiceText = choice.text().toLowerCase();
-            View.Action.toggleMenu(showingMenu);
+            V.Action.toggleMenu(showingMenu);
             if(choiceText === channel) return;
             channel = choiceText;
             currentPage = 1;
@@ -244,7 +250,7 @@
     tappable('#layout-option p', {
         onTap: function(e, target) {
             var choice = $(target);
-            View.Action.toggleMenu(showingMenu);
+            V.Action.toggleMenu(showingMenu);
             choice.toggleClass('two-icon');
             choice.toggleClass('three-icon');
             $('#mainWrap').empty();
@@ -292,7 +298,7 @@
     Poppp.loadShots();
     scrollTop();
 
-    View.Section.header.on('touchmove', function(e) {
+    V.Section.header.on('touchmove', function(e) {
         e.preventDefault();
     }, false);
 
